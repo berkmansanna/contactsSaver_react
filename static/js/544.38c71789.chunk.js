e04ee8beab8e2433d52141d3eccf8914648774dc
berkmansanna <90900129+berkmansanna@users.noreply.github.com>
2022-08-23 04:57:12 +0000

    Deploying to gh-pages from @ berkmansanna/goit-react-hw-08-phonebook@0b5d515620dd0b2d0f4a1125f442735d7590e752 🚀

---
+++ static/js/544.38c71789.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[544],{9544:function(e,o,n){n.r(o);var t=n(3329);o.default=function(){return(0,t.jsx)("div",{children:"home"})}}}]);
+//# sourceMappingURL=544.38c71789.chunk.js.map
